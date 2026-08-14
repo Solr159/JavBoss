7b4a96eee9437b9ef7c5b487c67e33c64c5a74e8
Solr159 <3296115337@qq.com>
2026-08-14 21:31:25 +0800

    x

--- web/src/components/JavTagModal.jsx
+++ web/src/components/JavTagModal.jsx
@@ -24,11 +24,11 @@ export default function JavTagModal(props) {
       tagLegend={[
         {
           label: zh('我创建的标签', 'My tags'),
-          className: 'border-emerald-200 bg-emerald-50',
+          className: 'border-emerald-200 bg-emerald-100',
         },
         {
           label: zh('抓取标签', 'Scraped tags'),
-          className: 'border-orange-200 bg-orange-50',
+          className: 'border-orange-200 bg-orange-100',
         },
       ]}
       editModeMessage={zh('只可编辑我创建的标签', 'Only my tags can be edited')}
